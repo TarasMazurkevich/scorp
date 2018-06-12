3030e05dd742548d4a1ff2be0f49866991b2431f
Taras <tvsy6520@gmail.com>
2018-06-12 16:43:04 +0300

    crutch for man-wrapper and crutch making for fire-wrapper

--- source/index.php
+++ source/index.php
@@ -1,14 +1,8 @@
-<?php 
-
-// ...
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Театр огня и света SCORPIUS | Киев, Украина</title>
 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,6 +14,7 @@
 	<meta name="theme-color" content="#000">
 
 	<!-- Поделючение внешних стилей -->
+	<link rel="stylesheet" href="css/lity.min.css">
 	<link rel="stylesheet" href="css/bootstrap-grid.min.css">
 	<link rel="stylesheet" href="css/main.css">
 
@@ -30,78 +25,84 @@
 	<![endif]-->
 </head>
 <body>
-	<div class="preloader"></div>
+	<!-- <div class="preloader"></div> -->
 
-	<div class="wrapper">
+	<!-- <div class="wrapper">
 		<header class="header">
-			<h1 class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci fugit dicta rem nobis beatae tempore.</h1>
+			<h1 class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
 		</header>
 		<div class="row">
-			<article class="col-6 wrapper__art">
+			<article class="col-sm-12 col-md-6 wrapper__art">
 				<h3 class="wrapper_title">Fire Show</h3>
 				<button class="wrapper_button">Выбрать</button>
 			</article>
-			<article class="col-6 wrapper__art">
+			<article class="col-sm-12 col-md-6 wrapper__art">
 				<h3 class="wrapper_title">Light Show</h3>
 				<button class="wrapper_button">Выбрать</button>
 			</article>
 		</div>
 		<footer class="wrapper__footer">
-			<a class="wrapper_link" href="tel:+380730516855">+38(073)-051-6855</a>
+			<a class="wrapper_phone" href="tel:+380730516855">+38(073)-051-6855</a>
 			<ul class="wrapper__list">
-				<li class="wrapper_soc"><span class="fab fa-instagram"></span></li>
-				<li class="wrapper_soc"><span class="fab fa-facebook-f"></span></li>
-				<li class="wrapper_soc"><span class="fas fa-at"></span></li>
-				<li class="wrapper_soc"><span class="fab fa-youtube"></span></li>
+				<li class="wrapper_soc"><a href="#"><span class="fab fa-instagram"></span></a></li>
+				<li class="wrapper_soc"><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+				<li class="wrapper_soc"><a href="#"><span class="fas fa-at"></span></a></li>
+				<li class="wrapper_soc"><a href="#"><span class="fab fa-youtube"></span></a></li>
 			</ul>
 		</footer>
-	</div>
+	</div> -->
 
-	<!-- <div class="fire-wrapper">
-		<div class="prog-block">
-			<div class="comeback"></div> Кнопка назад
-			<header>
-				<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, omnis beatae voluptatibus voluptates deleniti, nisi.</h2>
+	<div class="fire-wrapper">
+		<section class="programs">
+			<div class="comeback">Кнопка назад</div>
+			<header class="header programs__header">
+				<h2 class="title programs_title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, omnis beatae voluptatibus voluptates deleniti, nisi.</h2>
 			</header>
-			<article>
-				<h3>Moon Light</h3>
-				<iframe src="" frameborder="0"></iframe> video from youtube
-				Slider
-				<ul>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-				</ul>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis aut aspernatur at sunt numquam ut. Earum magni enim eos sint incidunt, deleniti blanditiis. Blanditiis ullam, quibusdam cum libero, illo rerum.</p>
-				<button>Выбрать</button>
-			</article>
-			<article>
-				<h3>Obsidian</h3>
-				<iframe src="" frameborder="0"></iframe> video from youtube
-				Slider
-				<ul>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-				</ul>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis aut aspernatur at sunt numquam ut. Earum magni enim eos sint incidunt, deleniti blanditiis. Blanditiis ullam, quibusdam cum libero, illo rerum.</p>
-				<button>Выбрать</button>
-			</article>
-		</div>
+			<div class="row">
+				<article class="col-12 col-md-6 programs__article">
+					<h3 class="programs_name">Moon Light</h3>
+					<div class="programs_promo">
+					 	<a href="https://www.youtube.com/embed/Kxjvsaws7UQ?rel=0" data-lity><img src="#" alt="iFrame from youtube"></a> <!-- video from youtube -->
+					</div>
+					<!-- Slider -->
+					<ul class="programs__slider">
+						<li class="programs_img"><img src="" alt="img1"></li>
+						<li class="programs_img"><img src="" alt="img2"></li>
+						<li class="programs_img"><img src="" alt="img3"></li>
+						<li class="programs_img"><img src="" alt="img4"></li>
+						<li class="programs_img"><img src="" alt="img5"></li>
+						<li class="programs_img"><img src="" alt="img6"></li>
+						<li class="programs_img"><img src="" alt="img7"></li>
+						<li class="programs_img"><img src="" alt="img8"></li>
+						<li class="programs_img"><img src="" alt="img9"></li>
+					</ul>
+					<p class="programs_desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis aut aspernatur at sunt numquam ut. Earum magni enim eos sint incidunt, deleniti blanditiis. Blanditiis ullam, quibusdam cum libero, illo rerum.</p>
+					<button class="programs_button">Выбрать</button>
+				</article>
+				<article class="col-12 col-md-6 programs__article">
+					<h3 class="programs_name">Obsidian</h3>
+					<div class="programs_promo">
+					 	<a href="https://www.youtube.com/embed/EHY2T0nyt6s?rel=0" data-lity><img src="#" alt="iFrame from youtube"></a> <!-- video from youtube -->
+					</div> 
+					<!-- Slider -->
+					<ul class="programs__slider">
+						<li class="programs_img"><img src="" alt="img1"></li>
+						<li class="programs_img"><img src="" alt="img2"></li>
+						<li class="programs_img"><img src="" alt="img3"></li>
+						<li class="programs_img"><img src="" alt="img4"></li>
+						<li class="programs_img"><img src="" alt="img5"></li>
+						<li class="programs_img"><img src="" alt="img6"></li>
+						<li class="programs_img"><img src="" alt="img7"></li>
+						<li class="programs_img"><img src="" alt="img8"></li>
+						<li class="programs_img"><img src="" alt="img9"></li>
+					</ul>
+					<p class="programs_desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis aut aspernatur at sunt numquam ut. Earum magni enim eos sint incidunt, deleniti blanditiis. Blanditiis ullam, quibusdam cum libero, illo rerum.</p>
+					<button class="programs_button">Выбрать</button>
+				</article>
+			</div>
+		</section>
 	
-		<div class="size-block">
+		<!-- <section class="size-block">
 			<div class="comeback"></div>
 			<header>
 				<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, impedit.</h2>
@@ -117,9 +118,9 @@
 					<li>5+</li> 5+ человек
 				</ul>
 			</article>
-		</div>
-	
-		<div class="add-block">
+		</section> -->
+			
+		<!-- <section class="add-block">
 			<div class="comeback"></div>
 			<header>
 				<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, hic.</h2>
@@ -135,9 +136,9 @@
 				</ul>
 			</article>
 			<button>Ничего ненадо</button>
-		</div>
-	
-		<div class="form-block">
+		</section> -->
+			
+		<!-- <section class="form-block">
 			<div class="comeback"></div>
 			<header>
 				<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, optio!</h2>
@@ -157,9 +158,9 @@
 					<input type="sumbit" value="Оформить заказ">
 				</form>
 			</article>
-		</div>
+		</section> -->
 	</div>
-	<div class="light-wrapper">
+	<!-- <div class="light-wrapper">
 		<div class="prog-block">
 			<div class="comeback"></div> Кнопка назад
 			<header>
@@ -248,6 +249,7 @@
 
 
 	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/lity.min.js"></script>
 	<script src="js/fontawesome-all.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<!-- <script src="js/jquery.touchSwipe.min.js"></script> -->
